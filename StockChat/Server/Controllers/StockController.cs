6d6fae2ae953654c3c92b1f3da65cb3b117e4198
David Pucheta <david.pucheta@scorpion.co>
2022-07-16 12:59:56 -0500

    Add project files.

---
+++ StockChat/Server/Controllers/StockController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StockChat.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private const string StockEndpoint = "https://stooq.com/q/l/?f=sd2t2ohlcv&h&e=csv&s=";
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public StockController(IHttpClientFactory httpClientFactory)
+        {
+           _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public IActionResult Get([FromQuery] string stockCode)
+        {
+            var stockUrl = StockEndpoint + stockCode;
+            var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri(stockUrl);
+
+            var result = client.GetStringAsync(stockUrl).Result;
+
+            var stockPrice = result.Split(',')[13];
+
+            if (!string.IsNullOrEmpty(stockPrice))
+            {
+                return Ok(stockPrice);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+    }
+}
---
+++ StockChat/Server/Hubs/ChatHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+namespace StockChat.Server.Hubs;
+
+public class ChatHub : Hub
+{
+    public const string HubUrl = "/chatroom";
+
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+
+    public async Task Broadcast(string username, string message)
+    {
+        await Clients.All.SendAsync("Broadcast", username, message);
+    }
+
+    public override Task OnConnectedAsync()
+    {
+        Console.WriteLine($"{Context.ConnectionId} connected");
+        return base.OnConnectedAsync();
+    }
+
+    public override async Task OnDisconnectedAsync(Exception? e)
+    {
+        Console.WriteLine($"Disconnected {e?.Message} {Context.ConnectionId}");
+        await base.OnDisconnectedAsync(e);
+    }
+}
